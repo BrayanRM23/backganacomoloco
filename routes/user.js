506f5c994b492d9d6dc02c8db8a8227590f2ac17
BrayanRM23 <brayan.rodriguez04@unicatolica.edu.co>
2024-11-01 17:18:55 -0500

    prueba1

--- routes/user.js
+++ routes/user.js
@@ -15,6 +15,27 @@ const UserSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'UserInfo',
         required: true
+    },
+    birthdate: {
+        type: Date,
+        required: true
+    },
+    cedula: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cellphone: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
     }
 });
 
